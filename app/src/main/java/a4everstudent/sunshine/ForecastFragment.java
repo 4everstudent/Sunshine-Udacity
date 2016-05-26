0bb3dfbe041fed01c56f9c054ec5cc1f1e89dec5
4EverStudent <mail@4everstudent.com>
2016-05-26 10:59:25 +0100

    inflate menu

--- app/src/main/java/a4everstudent/sunshine/ForecastFragment.java
+++ app/src/main/java/a4everstudent/sunshine/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -38,6 +41,10 @@ public class ForecastFragment extends Fragment {
     }
 
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -71,6 +78,19 @@ public class ForecastFragment extends Fragment {
         return rootView;
 
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        int id = item.getItemId();
+        if(id == R.id.action_refresh){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 
     public class FetchWeatherTask extends AsyncTask<Void, Void, Void> {
 
